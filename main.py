22f1c0e61e2961590e5f24fbfd242fac71733ba4
Nathan <152796887+nathandev0@users.noreply.github.com>
2024-03-30 18:12:18 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+TikTok Automation Bot (iOS, Android)
+Appium-based automated TikTok account creation, account modification and video uploader on real devices.
+
+Main functionality:
+1) Device & Automation management system
+2) Account creation (using email address or virtual phone number service API)
+3) Account modification (name, username, bio, link and profile picture)
+4) Video uploader (particular video from partucular album; video text, song and description;)
+5) IP rotation system (API proxy rotation link, Airplane mode)
+
+Additional information:
+1) Anti-bot system (human behavior modeling)
+2) No limit on devices / accounts (depending on your pc / laptop hardware)
+3) No jailbreak (iOS) / No root (Android) required
+4) Compatible with Windows and MAC OS
+
+Contact
+- https://t.me/nathan_dev
