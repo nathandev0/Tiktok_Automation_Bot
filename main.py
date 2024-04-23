fe89c79cd79be5f103e63a0c248b46c383af0db1
Nathan <152796887+nathandev0@users.noreply.github.com>
2024-04-23 15:12:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ TikTok Automation Bot (iOS, Android)
 Appium-based automated TikTok account creation, account modification and video uploader on real devices.
 
 Contact:
-https://t.me/nathan_dev0 (new account)
+https://t.me/nathan_dev0
